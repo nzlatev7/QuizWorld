9052c2444fb1a487883bf0c30b8f4dd212e41603
Daniel Dobrev <106867192+nzlatev7@users.noreply.github.com>
2022-11-21 10:05:14 +0200

    connecting string with db

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using QuizMarket;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<QuizWorldDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
