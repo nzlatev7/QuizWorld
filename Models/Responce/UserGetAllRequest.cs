a2f4efc99349204538850f87a96d2f7f420bff8c
Daniel Dobrev <>
2022-11-23 09:40:33 +0200

    make response model

---
+++ Models/Responce/UserGetAllRequest.cs
@@ -0,0 +1,12 @@
+﻿using QuizMarket.Models.DB;
+
+namespace QuizMarket.Models.Response
+{
+    public class UserGetAllResponce
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        //public IEnumerable<Quiz> ListOfQuiz { get; set; }
+    }
+}
