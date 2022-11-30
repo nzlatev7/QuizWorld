11a745e8cc49c5959112f33ebb1c90dbbee7a0f4
Nikolay <nikolay.zlatev2005@gmail.com>
2022-11-30 19:09:26 +0200

    make migration for addedRoles

--- Migrations/20221130153952_addedRoles.Designer.cs
+++ Migrations/20221130153952_addedRoles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizMarket;
 
@@ -11,9 +12,10 @@ using QuizMarket;
 namespace QuizWorld.Migrations
 {
     [DbContext(typeof(QuizWorldDbContext))]
-    partial class QuizWorldDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221130153952_addedRoles")]
+    partial class addedRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
